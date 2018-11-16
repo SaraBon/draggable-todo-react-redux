57ddffba044e1f2536123936b4d60b180e5bbcf7
SaraBon <sarabongartz123@gmail.com>
2018-11-16 20:46:48 +0100

    Updates

---
+++ precache-manifest.7b735f03081961af8e683a42ed8afae7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "41d598e2f9ad9d08c4c4",
+    "url": "/draggable-todo-react-redux/static/js/runtime~main.41d598e2.js"
+  },
+  {
+    "revision": "edfdf86e258688fed836",
+    "url": "/draggable-todo-react-redux/static/js/main.edfdf86e.chunk.js"
+  },
+  {
+    "revision": "6fe210ae693d70caa16c",
+    "url": "/draggable-todo-react-redux/static/js/1.6fe210ae.chunk.js"
+  },
+  {
+    "revision": "edfdf86e258688fed836",
+    "url": "/draggable-todo-react-redux/static/css/main.e4b007bb.chunk.css"
+  },
+  {
+    "revision": "12fd05fe0bc5ce2664ed3985a845bfb7",
+    "url": "/draggable-todo-react-redux/index.html"
+  }
+];
